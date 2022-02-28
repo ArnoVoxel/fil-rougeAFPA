a008e632b0f0a296c57d84eefbacd92ab15b181c
ArnoVoxel <jimmymoliere@gmail.com>
2022-02-28 10:01:04 +0100

    affichage dynamique BD partie category

---
+++ sources/js/affichage_dynamique.js
@@ -0,0 +1,53 @@
+//déclaration
+var objet_album = {};
+var id_serie = 0;
+var titre_album = "";
+var auteur_album = "";
+
+//attention aux accents dans les noms d'images
+//RegEx pour les supprimer dans la concaténation
+const regex = /['!?.":$]/g;
+
+
+afficher_BD_random();
+
+function choisir_BD_random() {
+    do {
+        //valeur aléatoire entre 1 et 629
+        var nb_random = parseInt(Math.random() * 629 + 1);
+    } while (!albums.get(nb_random.toString()));
+
+    //crée l'objet BD à partir du chiffre aléatorie
+    objet_album = albums.get(nb_random.toString());
+
+}
+
+function afficher_BD_random() {
+    //boucle qui crée le nombre de BD dans la partie category
+    //le nombre pourra être modifié par l'utilisateur
+    for (let i = 0; i < 4; i++) {
+
+        choisir_BD_random();
+        //création div qui contiendra image, titre, prix et bouton infos
+        var col_card_BD = document.createElement('div');
+        col_card_BD.setAttribute('class', 'col-6 col-md-3');
+        col_card_BD.setAttribute('id', 'col_card_BD_category' + i);
+        document.getElementById('show_bd').appendChild(col_card_BD);
+
+        var image_card_BD = document.createElement('img');
+        image_card_BD.setAttribute('class', 'img-fluid image_grid');
+        image_card_BD.setAttribute('src', '../sources/albums/' + series.get(objet_album.idSerie).nom.replace(regex, "") + '-' + objet_album.numero + '-' + objet_album.titre.replace(regex, "") + '.jpg');
+        document.getElementById('col_card_BD_category' + i).appendChild(image_card_BD);
+
+        var infos_card_BD = document.createElement('div');
+        infos_card_BD.textContent = objet_album.titre + ', prix : ' + objet_album.prix;
+        document.getElementById('col_card_BD_category' + i).appendChild(infos_card_BD);
+
+        var btn_card_BD = document.createElement('button');
+        btn_card_BD.setAttribute('id', 'bouton_panier' + i);
+        btn_card_BD.textContent = "ajouter au panier";
+        document.getElementById('col_card_BD_category' + i).appendChild(btn_card_BD);
+    }
+
+
+}

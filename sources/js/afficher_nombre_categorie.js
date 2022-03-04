5e732f65fe2872ab1151f7a3937ddc41a425025f
ArnoVoxel <jimmymoliere@gmail.com>
2022-03-04 12:48:10 +0100

    navigation par tri fonctionnelle

--- sources/js/afficher_nombre_categorie.js
+++ sources/js/afficher_nombre_categorie.js
@@ -1,10 +1,3 @@
-//déclaration
-const select_nombre = document.querySelector('.nombre_bd');
-const select_category = document.querySelector('.category');
-
-select_nombre.addEventListener('change', affichage_elements_category);
-select_category.addEventListener('change', affichage_contenu_category);
-
 /**
  * afficher le nombre d'éléments avec le select
  */
@@ -13,5 +6,25 @@ function affichage_elements_category(e) {
 }
 
 function affichage_contenu_category(e) {
+    document.getElementById('body_page').textContent = '';
+    creer_liste_tri();
+    creer_liste_nombre_affichage();
+
+    var bd_container = document.createElement('div');
+    bd_container.setAttribute('id', 'bd_container');
+    bd_container.setAttribute('class', 'col-12 col-md-10');
+    document.getElementById('body_page').appendChild(bd_container);
+
+    // console.log(e);
+    // console.log(e.target);
+    // console.log(JSON.parse(sessionStorage.getItem(e.target.value)));
+
+
+    //prendre en compte les valeurs series et auteur
+    // if (JSON.parse(sessionStorage.getItem(e.target.value)) == null) {
+    //     afficher_BD_random(JSON.parse(sessionStorage.getItem('last')), 4);
+    // } else {
     afficher_BD_random(JSON.parse(sessionStorage.getItem(e.target.value)), 4);
+    //}
+
 }
